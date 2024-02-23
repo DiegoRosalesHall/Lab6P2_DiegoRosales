da3dd74a86c28037f250f9059fbf29383050f1ac
DiegoRosalesHall <diegogerardorhall2@Gmail.com>
2024-02-23 13:16:36 -0600

    Commit inicial

---
+++ src/lab6p2_diegorosales/Lab6P2_DiegoRosales.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab6p2_diegorosales;
+
+/**
+ *
+ * @author Diego
+ */
+public class Lab6P2_DiegoRosales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
